4d6621016a11a3324f4df93419cdb52629c8b204
AryanCCTech <aryan.yadav@cctech.co.in>
2024-10-28 14:06:54 +0530

    changes

--- Visualizer/headers/Triangulation.h
+++ Visualizer/headers/Triangulation.h
@@ -11,8 +11,4 @@ public:
 
     std::vector<double> UniqueNumbers;
     std::vector<Triangle> Triangles;
-    std::vector<Point> uniqueVertex;
-    std::vector<Point> uniqueNormal;
-    std::map<Point, int> uniqueVertexMap;
-    std::map<Point, int> uniqueNormalMap;
 };
